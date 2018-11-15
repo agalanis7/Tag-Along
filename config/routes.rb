b29af189e4cf8978e1d64f39108e7397599d00f0
Melidios One <42850434+Melidios@users.noreply.github.com>
2018-11-15 16:48:08 -0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  get 'welcome/home'
-  devise_for :users
-  root 'events#index'
-=======
   root 'welcome#show'
   resources :welcome
   devise_for :users
->>>>>>> master
   resources :events, except: [:show ]
   resources :profiles
   resources :activities
-
   resources :locations, only: [:index]
   get 'location/index'
 
